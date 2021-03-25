420f213eea9585286e72c628028b8919791415ae
nicolai1233 <32257639+nicolai1233@users.noreply.github.com>
2021-03-25 15:54:39 +0100

    added firebase and a new activity. it is also possible to move from MainActivity to MainScreen by pressing logIn.

--- app/src/main/java/com/example/tabletappv2/MainActivity.java
+++ app/src/main/java/com/example/tabletappv2/MainActivity.java
@@ -2,13 +2,57 @@ package com.example.tabletappv2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mPassword,mUsername;
+    private Button mLogin;
+    private FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fAuth = FirebaseAuth.getInstance();
+        createWigedt();
+
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToMainScreen();
+            }
+        });
+
+
+
     }
+
+
+    public void login(){
+
+    }
+
+
+    public void goToMainScreen(){
+        Intent intent = new Intent(this, MainScreen.class);
+        startActivity(intent);
+    }
+
+
+    public void createWigedt(){
+        mPassword = findViewById(R.id.loginPasswordPt);
+        mUsername = findViewById(R.id.loginUsernamePT);
+        mLogin = findViewById(R.id.loginBtn);
+
+
+
+    }
+
+
 }
